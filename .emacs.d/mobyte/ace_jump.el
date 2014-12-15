a7cc7799406fd581e8d76440774306ea85711338
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2014-12-15 16:50:25 +0100

    Add ability to use 'ace-jump' in shell mode.

--- .emacs.d/mobyte/ace_jump.el
+++ .emacs.d/mobyte/ace_jump.el
@@ -1,8 +1,6 @@
 (require 'ace-jump-mode)
 (global-set-key (kbd "C-c SPC") 'ace-jump-char-mode)
-;; (global-set-key (kbd "C-u C-c SPC") 'ace-jump-mode)
-;; (global-set-key (kbd "C-x o") 'ace-jump-)
 ;;(define-key global-map (kbd "C-x o") 'other-window)
 
-
-
+(eval-after-load 'shell
+  '(define-key shell-mode-map (kbd "C-c SPC") 'ace-jump-char-mode))
