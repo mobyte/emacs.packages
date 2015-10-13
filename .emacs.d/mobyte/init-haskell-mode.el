1d0a8503da716ec1df16b68e81fbe1794c2b1366
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2015-10-13 16:19:56 +0200

    Vanished everything.

--- .emacs.d/mobyte/init-haskell-mode.el
+++
@@ -1,4 +0,0 @@
-(load "/home/mobyte/src/emacs/haskell-mode-2.4/haskell-site-file")
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
--- .emacs.d/mobyte/load-fullscreen.el
+++
@@ -1,4 +0,0 @@
-(load-file "~/.emacs.d/fullscreen.el")
-(require 'fullscreen)
-
-(global-set-key (kbd "C-c f") 'fullscreen-toggle)
