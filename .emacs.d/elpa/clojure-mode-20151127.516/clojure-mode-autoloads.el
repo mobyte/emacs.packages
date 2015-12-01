0c516106593fb6506b74c1684e22c313172080b5
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2015-12-01 10:45:06 +0100

    updated clojure-mode.

--- .emacs.d/elpa/clojure-mode-20151127.516/clojure-mode-autoloads.el
+++ .emacs.d/elpa/clojure-mode-20151127.516/clojure-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (22098 49105
+;;;### (autoloads nil "clojure-mode" "clojure-mode.el" (22109 27615
 ;;;;;;  0 0))
 ;;; Generated autoloads from clojure-mode.el
 
