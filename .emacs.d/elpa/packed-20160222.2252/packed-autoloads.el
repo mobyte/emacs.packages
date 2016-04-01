4031aebe47b6f7076a956edc2927f48364201811
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2016-04-01 08:11:26 +0200

    Upgraded installed packages.

--- .emacs.d/elpa/packed-20160222.2252/packed-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; packed-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("packed.el") (22220 3342 575049 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; packed-autoloads.el ends here
