7ebac0ff5c8261cb6d8f16e3021a522b174b9a28
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2016-05-18 08:48:15 +0200

    Upgraded installed packages.

--- .emacs.d/elpa/magit-20160517.838/magit-pkg.el
+++ .emacs.d/elpa/magit-20160517.838/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20160514.1451" "A Git porcelain inside Emacs"
+(define-package "magit" "20160517.838" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20150909.2257")
     (dash "20151021.113")
