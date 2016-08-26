bc3ec180fcbc8f040bdc6f2288a2fd8f1c7b7e70
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2016-08-26 10:51:00 +0200

    Upgraded packages.

--- .emacs.d/elpa/hydra-20160825.36/hydra-autoloads.el
+++ .emacs.d/elpa/hydra-20160825.36/hydra-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "hydra" "hydra.el" (22393 64714 0 0))
+;;;### (autoloads nil "hydra" "hydra.el" (22463 62525 0 0))
 ;;; Generated autoloads from hydra.el
 
 (autoload 'defhydra "hydra" "\
@@ -63,7 +63,7 @@ result of `defhydra'.
 ;;;***
 
 ;;;### (autoloads nil nil ("hydra-examples.el" "hydra-ox.el" "hydra-pkg.el"
-;;;;;;  "lv.el") (22393 64714 234847 0))
+;;;;;;  "lv.el") (22463 62525 535539 0))
 
 ;;;***
 
