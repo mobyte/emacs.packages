c2e4b6b284795fb9f675355f3e58b462e08d90f0
Emil Yessenamanov <mobyte@digital0320.local>
2017-03-01 10:22:03 +0100

    Upgraded packages

---
+++ .emacs.d/elpa/frame-fns-20170222.1759/frame-fns-autoloads.el
@@ -0,0 +1,15 @@
+;;; frame-fns-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("frame-fns.el") (22710 37434 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; frame-fns-autoloads.el ends here
