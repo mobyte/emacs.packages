fbef9079d433fe687dd6d1709cd8e65bcbaf803a
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2017-03-16 14:43:28 +0100

    Upgraded packages.

---
+++ .emacs.d/elpa/zoom-frm-20170309.1439/zoom-frm-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zoom-frm" "20170309.1439" "Commands to zoom frame font size." '((frame-fns "0") (frame-cmds "0")) :url "https://www.emacswiki.org/emacs/download/zoom-frm.el" :keywords '("frames" "extensions" "convenience"))
