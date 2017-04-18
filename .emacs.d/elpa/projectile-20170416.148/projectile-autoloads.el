2f39fd05d70186b9c422b412b5cd9c0536d7f6af
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2017-04-18 09:27:30 +0200

    Upgraded packages.

--- .emacs.d/elpa/projectile-20170416.148/projectile-autoloads.el
+++ .emacs.d/elpa/projectile-20170416.148/projectile-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "projectile" "projectile.el" (22763 18918 0
+;;;### (autoloads nil "projectile" "projectile.el" (22773 48846 0
 ;;;;;;  0))
 ;;; Generated autoloads from projectile.el
 
