61362935a19f316f3d5b97db8f72edf1b46e071d
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2017-05-18 16:28:03 +0200

    Upgraded packages.

--- .emacs.d/elpa/with-editor-20170517.1242/with-editor-autoloads.el
+++ .emacs.d/elpa/with-editor-20170517.1242/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22773 48840 0 0))
+;;;;;;  (22813 44668 0 0))
 
 ;;;***
 
