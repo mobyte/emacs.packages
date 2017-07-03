7088143718e7c41fe5e894c8f6f8f66401d382cc
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2017-07-03 14:36:34 +0200

    Upgraded packages.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,23 +1,23 @@
 {:user
  {;; :plugins [[refactor-nrepl "2.3.0-SNAPSHOT"]]
-  :repl-options {:nrepl-middleware
-                 [cider.nrepl.middleware.apropos/wrap-apropos
-                  cider.nrepl.middleware.classpath/wrap-classpath
-                  cider.nrepl.middleware.complete/wrap-complete
-                  cider.nrepl.middleware.debug/wrap-debug
-                  cider.nrepl.middleware.format/wrap-format
-                  cider.nrepl.middleware.info/wrap-info
-                  cider.nrepl.middleware.inspect/wrap-inspect
-                  cider.nrepl.middleware.macroexpand/wrap-macroexpand
-                  cider.nrepl.middleware.ns/wrap-ns
-                  cider.nrepl.middleware.pprint/wrap-pprint
-                  cider.nrepl.middleware.resource/wrap-resource
-                  cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                  cider.nrepl.middleware.test/wrap-test
-                  cider.nrepl.middleware.trace/wrap-trace
-                  cider.nrepl.middleware.undef/wrap-undef
-                  refactor-nrepl.middleware/wrap-refactor]}
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+  ;; :repl-options {:nrepl-middleware
+  ;;                [cider.nrepl.middleware.apropos/wrap-apropos
+  ;;                 cider.nrepl.middleware.classpath/wrap-classpath
+  ;;                 cider.nrepl.middleware.complete/wrap-complete
+  ;;                 cider.nrepl.middleware.debug/wrap-debug
+  ;;                 cider.nrepl.middleware.format/wrap-format
+  ;;                 cider.nrepl.middleware.info/wrap-info
+  ;;                 cider.nrepl.middleware.inspect/wrap-inspect
+  ;;                 cider.nrepl.middleware.macroexpand/wrap-macroexpand
+  ;;                 cider.nrepl.middleware.ns/wrap-ns
+  ;;                 cider.nrepl.middleware.pprint/wrap-pprint
+  ;;                 cider.nrepl.middleware.resource/wrap-resource
+  ;;                 cider.nrepl.middleware.stacktrace/wrap-stacktrace
+  ;;                 cider.nrepl.middleware.test/wrap-test
+  ;;                 cider.nrepl.middleware.trace/wrap-trace
+  ;;                 cider.nrepl.middleware.undef/wrap-undef
+  ;;                 refactor-nrepl.middleware/wrap-refactor]}
+  :dependencies [;; [org.clojure/tools.nrepl "0.2.12"]
                  [spyscope "0.1.5"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [leiningen #=(leiningen.core.main/leiningen-version)]
