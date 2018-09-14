d28ae4d9fa379cdad0a0a737088f5a44f18a7e18
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2018-09-14 13:38:28 +0200

    upgraded packages

--- .emacs.d/elpa/company-20180913.2311/company-pkg.el
+++ .emacs.d/elpa/company-20180913.2311/company-pkg.el
@@ -1,4 +1,4 @@
-(define-package "company" "20180910.1342" "Modular text completion framework"
+(define-package "company" "20180913.2311" "Modular text completion framework"
   '((emacs "24.3"))
   :keywords
   '("abbrev" "convenience" "matching")
