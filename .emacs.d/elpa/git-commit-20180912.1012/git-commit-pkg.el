03eba4e728c38f703c812f4496d43f43e4dde281
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2018-10-30 14:50:34 +0100

    upgraded packages

--- .emacs.d/elpa/git-commit-20180912.1012/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20180912.1012" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "d913880e165b162420347cbcf10a40dc21094248" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
