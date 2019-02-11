6f184f32799445b5d5432bc6747daad1d1067583
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2019-02-11 10:27:54 +0100

    Upgraded clojure version to 1.10 in .lein/profiles.clj.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.20.1-SNAPSHOT"]]
+ {:dependencies [[org.clojure/clojure "1.10.0"]]
+  :plugins [[cider/cider-nrepl "0.20.1-SNAPSHOT"]]
   :middleware [cider-nrepl.plugin/middleware]
   :jvm-opts ["-Djsse.enableSNIExtension=false"]
   :java-cmd "/Users/mobyte/src/jdk-11.0.1.jdk/Contents/Home/bin/java"}}
