82173fda9041eeb5033520fa0f967340f5bae5d2
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2019-03-07 15:45:03 +0100

    upgraded packages

--- .emacs.d/elpa/magit-20190307.1247/magit-pkg.el
+++ .emacs.d/elpa/magit-20190307.1247/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20190306.413" "A Git porcelain inside Emacs."
+(define-package "magit" "20190307.1247" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180910")
