d9c0febef82b6c52a8a0026cc5591e1a2317dd6a
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2019-04-25 14:28:15 +0200

    upgrade packages

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,4 +3,4 @@
   :plugins [[cider/cider-nrepl "0.21.0"]]
   :middleware [cider-nrepl.plugin/middleware]
   :jvm-opts ["-Djsse.enableSNIExtension=false"]
-  :java-cmd "/Users/mobyte/src/jdk-11.0.1.jdk/Contents/Home/bin/java"}}
+  :java-cmd "/Users/mobyte/src/jdk-12.0.1.jdk/Contents/Home/bin/java" #_"/Users/mobyte/src/jdk-11.0.1.jdk/Contents/Home/bin/java"}}
