bd05a6c86738d85aae06322fe6a81b268b4e60bf
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2019-04-26 09:23:34 +0200

    upgrade packages

--- .emacs.d/elpa/clj-refactor-20190425.1002/clj-refactor-pkg.el
+++ .emacs.d/elpa/clj-refactor-20190425.1002/clj-refactor-pkg.el
@@ -1,4 +1,4 @@
-(define-package "clj-refactor" "20190418.1411" "A collection of commands for refactoring Clojure code"
+(define-package "clj-refactor" "20190425.1002" "A collection of commands for refactoring Clojure code"
   '((emacs "25.1")
     (seq "2.19")
     (yasnippet "0.6.1")
