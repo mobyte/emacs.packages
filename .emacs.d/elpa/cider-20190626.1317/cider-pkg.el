0a9323ab18e7daebf410cfcca832d24ebf41372e
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2019-06-27 10:08:02 +0200

    upgraded packages

--- .emacs.d/elpa/cider-20190626.1317/cider-pkg.el
+++ .emacs.d/elpa/cider-20190626.1317/cider-pkg.el
@@ -1,4 +1,4 @@
-(define-package "cider" "20190625.1651" "Clojure Interactive Development Environment that Rocks"
+(define-package "cider" "20190626.1317" "Clojure Interactive Development Environment that Rocks"
   '((emacs "25")
     (clojure-mode "5.9")
     (parseedn "0.1")
