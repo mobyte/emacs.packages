5facdc8e669869e8ec26f9422080bcf642b8bfaa
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2019-08-07 10:23:18 +0200

    upgrade packages

--- .emacs.d/elpa/transient-20190806.1004/transient-pkg.el
+++ .emacs.d/elpa/transient-20190806.1004/transient-pkg.el
@@ -1,4 +1,4 @@
-(define-package "transient" "20190630.1359" "Transient commands"
+(define-package "transient" "20190806.1004" "Transient commands"
   '((emacs "25.1")
     (dash "2.15.0"))
   :keywords
