2e52f791e9ac58d5d1e6985ad4ea7a847649b9de
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2019-08-12 14:59:28 +0200

    upgraded packages

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:dependencies [[org.clojure/clojure "1.10.1"]]
-  :plugins [[cider/cider-nrepl "0.22.0-beta8"]
+  :plugins [[cider/cider-nrepl "0.22.0-beta9"]
             [refactor-nrepl "2.5.0-SNAPSHOT"]]
   :repl-options {:init (do (require 'clojure.pprint
                                     'clojure.tools.namespace.repl)
@@ -9,7 +9,9 @@
                            (intern 'clojure.core
                                    '>refresh clojure.tools.namespace.repl/refresh)
                            (intern 'clojure.core
-                                   '>clear clojure.tools.namespace.repl/clear))
+                                   '>clear clojure.tools.namespace.repl/clear)
+                           (intern 'clojure.core
+                                   '>run-tests clojure.test/run-tests))
                  :skip-default-init true
                  :host "0.0.0.0"
                  :port 4001}
