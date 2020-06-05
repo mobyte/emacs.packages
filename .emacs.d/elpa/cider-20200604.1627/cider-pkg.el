a11dba9619308146654b0b1260cac270c56367ab
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2020-06-05 16:30:12 +0200

    upgraded packages

--- .emacs.d/elpa/cider-20200604.1627/cider-pkg.el
+++ .emacs.d/elpa/cider-20200604.1627/cider-pkg.el
@@ -1,4 +1,4 @@
-(define-package "cider" "20200604.912" "Clojure Interactive Development Environment that Rocks"
+(define-package "cider" "20200604.1627" "Clojure Interactive Development Environment that Rocks"
   '((emacs "25")
     (clojure-mode "5.11")
     (parseedn "0.1")
@@ -7,7 +7,7 @@
     (spinner "1.7")
     (seq "2.16")
     (sesman "0.3.2"))
-  :commit "c5d9b18ad495db64b98c06e01b235aa33e3fe148" :keywords
+  :commit "1911c474baefa1ef0f50274c7035a46949249d58" :keywords
   '("languages" "clojure" "cider")
   :authors
   '(("Tim King" . "kingtim@gmail.com")
--- .emacs.d/elpa/magit-20200604.1359/magit-pkg.el
+++ .emacs.d/elpa/magit-20200604.1359/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20200603.209" "A Git porcelain inside Emacs."
+(define-package "magit" "20200604.1359" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180910")
     (git-commit "20181104")
     (transient "20190812")
     (with-editor "20181103"))
-  :commit "12f0878b6c772893ad5bcbe3fb782baa2d216e24" :keywords
+  :commit "6b88fab39618c6a82c2b2c7b11f4398f924c437a" :keywords
   '("git" "tools" "vc"))
 ;; Local Variables:
 ;; no-byte-compile: t
