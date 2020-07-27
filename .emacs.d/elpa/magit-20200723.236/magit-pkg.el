b207602da6ccf4e19d7a0ad02afdd9aa187dd50e
Emil Yessenamanov <emil.yessenamanov@rewe-digital.com>
2020-07-27 09:03:38 +0200

    upgraded packages

--- .emacs.d/elpa/magit-20200723.236/magit-pkg.el
+++ .emacs.d/elpa/magit-20200723.236/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20200713.2103" "A Git porcelain inside Emacs."
+(define-package "magit" "20200723.236" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20200113")
     (dash "20200524")
     (git-commit "20200516")
     (transient "20200601")
     (with-editor "20200522"))
-  :commit "dc8a24b4cdb4c6371d3f52a5105f9887bbcc371b" :keywords
+  :commit "9ef75c1098a525a0c817a164516e67c8a13cfca7" :keywords
   '("git" "tools" "vc"))
 ;; Local Variables:
 ;; no-byte-compile: t
