1a78b877d6ceaae4d0de6bc629a110ef9a6ff9b3
Emil Yessenamanov <mobyte@gmail.com>
2020-08-04 19:29:47 +0200

    upgraded packages

--- .emacs.d/elpa/cider-20200803.831/cider-pkg.el
+++ .emacs.d/elpa/cider-20200803.831/cider-pkg.el
@@ -1,4 +1,4 @@
-(define-package "cider" "20200718.1908" "Clojure Interactive Development Environment that Rocks"
+(define-package "cider" "20200803.831" "Clojure Interactive Development Environment that Rocks"
   '((emacs "25")
     (clojure-mode "5.11")
     (parseedn "0.1")
@@ -7,7 +7,7 @@
     (spinner "1.7")
     (seq "2.16")
     (sesman "0.3.2"))
-  :commit "3d8552a8469a5515cd6e783b5a9ab09ba9d51eb5" :keywords
+  :commit "9e117c2ff34b7a63eee87c8bfa93a800bb94add5" :keywords
   '("languages" "clojure" "cider")
   :authors
   '(("Tim King" . "kingtim@gmail.com")
