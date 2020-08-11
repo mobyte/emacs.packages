4424ae46c4739d0dcc8070b1c7d92157090f1656
Emil Yessenamanov <mobyte@gmail.com>
2020-08-11 13:54:54 +0200

    upgraded packages

---
+++ .emacs.d/elpa/projectile-20200810.544/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile" "20200810.544" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "fc9d4c8afd406fa46a686b999a7b8f5701614442" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
