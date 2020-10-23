b791e05f527d1a27f659c2c0d0ab58731fa796e9
Emil Yessenamanov <mobyte@gmail.com>
2020-10-23 13:23:58 +0200

    upgraded packages

---
+++ .emacs.d/elpa/cider-20201020.1058/cider-pkg.el
@@ -0,0 +1,24 @@
+(define-package "cider" "20201020.1058" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "25")
+    (clojure-mode "5.12")
+    (parseedn "0.2")
+    (pkg-info "0.4")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.16")
+    (sesman "0.3.2"))
+  :commit "8ee6dcc2cdaf6461539a2f0a2bb7721633a1b08c" :keywords
+  '("languages" "clojure" "cider")
+  :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.com")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.com")
+  :url "http://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
