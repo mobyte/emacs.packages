1bc2ad4bdd47a8e2df90cd4a0c0d48ea3fdb1ccc
Emil Yessenamanov <mobyte@gmail.com>
2020-11-27 08:55:45 +0100

    upgraded packages

---
+++ .emacs.d/elpa/magit-20201113.1245/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20201113.1245" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (async "20200113")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "4b9c2d26a6aef09a9f3c35e364dacd3739562963" :keywords
+  ("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
