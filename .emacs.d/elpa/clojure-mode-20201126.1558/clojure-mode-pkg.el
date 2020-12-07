3ac1b18fe40e8d8c6a0e771d236e43fbe674fb0b
Emil Yessenamanov <mobyte@gmail.com>
2020-12-07 16:29:56 +0100

    upgraded packages

---
+++ .emacs.d/elpa/clojure-mode-20201126.1558/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/mobyte/.emacs.d/elpa/clojure-mode-20201126.1558/clojure-mode.el  -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "20201126.1558" "Major mode for Clojure code" '((emacs "25.1")) :commit "53ef8ac076ae7811627fbdd408e519ab7fca9a0b" :keywords '("languages" "clojure" "clojurescript" "lisp") :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "http://github.com/clojure-emacs/clojure-mode")
