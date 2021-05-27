233f1106186efb3457dc9dcf4e45c42f17e767a9
Emil Yessenamanov <mobyte@gmail.com>
2021-05-27 09:44:18 +0200

    upgraded packages

--- .emacs.d/elpa/company-20210513.1853/company-pkg.el
+++ .emacs.d/elpa/company-20210513.1853/company-pkg.el
@@ -1,6 +1,6 @@
-(define-package "company" "20210509.2341" "Modular text completion framework"
+(define-package "company" "20210513.1853" "Modular text completion framework"
   '((emacs "25.1"))
-  :commit "f12295966e552a397349126a75dbc93e4fedc7ec" :authors
+  :commit "34862485c6b8badc98fee29596fe56723dcf6f77" :authors
   '(("Nikolaj Schumacher"))
   :maintainer
   '("Dmitry Gutov" . "dgutov@yandex.ru")
--- .emacs.d/elpa/magit-20210512.1949/magit-pkg.el
+++ .emacs.d/elpa/magit-20210512.1949/magit-pkg.el
@@ -1,10 +1,10 @@
-(define-package "magit" "20210430.404" "A Git porcelain inside Emacs."
+(define-package "magit" "20210512.1949" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (dash "20200524")
     (git-commit "20200516")
     (transient "20200601")
     (with-editor "20200522"))
-  :commit "471c63d92ce22b8ea653f821bc1893ecea324d4d" :authors
+  :commit "fcd50dd8ae7cb33332eb7c6d90932be41b869cae" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
