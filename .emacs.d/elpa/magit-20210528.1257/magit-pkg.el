64848165b80d60e0cffdb4bcd3687adc81ac1a10
Emil Yessenamanov <mobyte@gmail.com>
2021-06-15 09:00:14 +0200

    upgraded packages

--- .emacs.d/elpa/magit-20210528.1257/magit-pkg.el
+++ .emacs.d/elpa/magit-20210528.1257/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20210525.844" "A Git porcelain inside Emacs."
+(define-package "magit" "20210528.1257" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (dash "20210330")
     (git-commit "20210525")
     (magit-section "20210525")
     (transient "20210524")
     (with-editor "20210524"))
-  :commit "b68a760c9e7694c687adedec7dffab0a5609ea93" :authors
+  :commit "f50c3aa56704cddd46ca987722642584bc8290b0" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
