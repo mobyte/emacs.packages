c4c49db9ef24fca3e85e37dba33c695df792a33f
Emil Yessenamanov <mobyte@gmail.com>
2022-04-07 17:30:05 +0200

    upgraded emacs packages

---
+++ .emacs.d/elpa/parseclj-20220328.558/parseclj-pkg.el
@@ -0,0 +1,11 @@
+(define-package "parseclj" "20220328.558" "Clojure/EDN parser"
+  '((emacs "25"))
+  :commit "b04eae67384c1d8181edf318fd62d422a8220724" :authors
+  '(("Arne Brasseur" . "arne@arnebrasseur.net"))
+  :maintainer
+  '("Arne Brasseur" . "arne@arnebrasseur.net")
+  :keywords
+  '("lisp" "clojure" "edn" "parser"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
